051ea5a794fbf632cdcecf777966dfed7c6ed403
you-know-who-2017 <1376397133@qq.com>
2019-12-12 20:45:44 +0800

    update

--- ProgrammingInLua/ch01/libl.lua
+++ ProgrammingInLua/ch01/libl.lua
@@ -5,3 +5,17 @@ end
 function twice (x)
     return 2.0 * x
 end
+
+--- dofile("lib1.lua")  in  lua
+
+--[[
+print(10)
+--]]
+
+---[[
+print(10)
+--]]
+
+
+--- lua -e "print(math.sin(12))"
+--- lua -e "sin=math.sin" script a b
